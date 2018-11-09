8d03e64c3944e5a49d25969a3e5d595c36d6c32b
Akanksha <agaur05@gmail.com>
2018-11-08 20:08:38 -0600

    adding api route to server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3030;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 
 require('./routes/htmlRoutes')(app);
+require('./routes/apiroutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
