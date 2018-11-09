45983f668491daa2f671a4d60db4b109983ea7d9
MattsPoche <MatthewsPoche@comcast.net>
2018-11-08 19:41:39 -0600

    changes

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
