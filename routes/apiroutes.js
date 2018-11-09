6d45bb94ceed0e35e133b2dfb5b50cf480c39f21
Roger <rgbyford@gmail.com>
2018-11-08 20:11:35 -0600

    V2

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -11,14 +11,20 @@ module.exports = function(app) {
     let aoTables = [{}];
     // returns table info
     app.get("/tables", function(req, res) {
-        fs.readFile(path.join(__dirname, "app/data/tableDat.json"), data);
+        fs.readFile(path.join(__dirname, "app/data/tableData.json"), data);
         aoTables = JSON.parse(data);
         return res.json(aoTables);
     });
     
     app.post ("/reservation", function (req, res) {
+        if (aoTables.length === 0) {
+            // If no data (/tables hasn't been called), try reading it
+            // There may be no data there, but that's OK
+            fs.readFile(path.join(__dirname, "app/data/tableData.json"), data);
+            aoTables = JSON.parse(data);
+        }
         aoTables.push (req.body);
-        fs.writeFile(path.join(__dirname, "app/data/tableDat.json"),
+        fs.writeFile(path.join(__dirname, "app/data/tableData.json"),
          JSON.stringify(aoTables),
         function (err) {
             console.log(err)
