9ba3fde295d7991c0fa13a42c5f676db89d8085b
Roger <rgbyford@gmail.com>
2018-11-08 19:52:23 -0600

    first attempt

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,46 +5,23 @@ Table = {
     customerID: ""
 };
 
+var path = require ('path');
+
 module.exports = function(app) {
     let aoTables = [{}];
     // returns table info
     app.get("/tables", function(req, res) {
+        fs.readFile(path.join(__dirname, "app/data/tableDat.json"), data);
+        aoTables = JSON.parse(data);
         return res.json(aoTables);
     });
     
-    app.post ("/table", function (req, res) {
-        aoTables.push = req.body;
+    app.post ("/reservation", function (req, res) {
+        aoTables.push (req.body);
+        fs.writeFile(path.join(__dirname, "app/data/tableDat.json"),
+         JSON.stringify(aoTables),
+        function (err) {
+            console.log(err)
+        });
     });
-    
-    // Displays a single character, or returns false
-    app.get("/api/characters/:character", function(req, res) {
-        const chosen = req.params.character;
-    
-        console.log(chosen);
-    
-        for (let i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
-        }
-        }
-    
-        return res.json(false);
-    });
-    
-    // Create New Characters - takes in JSON input
-    app.post("/api/characters", function(req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
-       const newcharacter = req.body;
-    
-        // Using a RegEx Pattern to remove spaces from newCharacter
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
-    
-        console.log(newcharacter);
-    
-        characters.push(newcharacter);
-    
-        res.json(newcharacter);
-    });
-};
+}
